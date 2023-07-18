1b3a0da5d6e48389ff8dcdbc8811168c844a21f9
Lucy <lucy@tjlabscorp.com>
2023-07-18 16:33:25 +0900

    feat: upgrade streamlit charts

--- main.py
+++ main.py
@@ -8,26 +8,64 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import altair as alt
 
-def showFirstPage():
+def set_page_title():
     st.set_page_config(page_title="Jupiter Health Check System", page_icon=":desktop_computer:", layout="wide")
     st.title(":desktop_computer: Jupiter Health Check System")
 
+@st.cache_data()
+def upload_today_data():
+    current_time = datetime.now()
+    current_time_utc = current_time.astimezone(pytz.utc)
+    current_time_str = current_time_utc.isoformat()
+    current_time_json_data = json.dumps(current_time_str)   
+    data = {'start_time': current_time_json_data, 'sector_id': 6}
+    response_data = requests.get("http://localhost:8502/", data=json.dumps(data))
+
+    if response_data.status_code == 200:
+        response_data = response_data.json()
+        realtime_stat = response_data['currentStats']
+        cumulative_data_count = realtime_stat['total_data']
+        correction_rate = 100 - realtime_stat['threshold_10'] - realtime_stat['threshold_30'] - realtime_stat['threshold_50']
+
+        st.header("Daily Status")
+        col1, col2, col3 = st.columns(3)
+
+        if 'count' not in st.session_state:
+            st.session_state['count'] = cumulative_data_count
+        if 'rate' not in st.session_state:
+            st.session_state['rate'] = correction_rate
+
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+        col1.write("latest update time")
+        col1.markdown(f"<span style='font-size: 40px;'>{current_time}</span>", unsafe_allow_html=True)
+        col2.metric("cumulative_data_count", f"{cumulative_data_count} count", f"{cumulative_data_count - st.session_state['count']}count")
+        col3.metric("correction_rate", f"{correction_rate}%", f"{correction_rate - st.session_state['rate']}%")
+        st.session_state['count'] = cumulative_data_count
+        st.session_state['rate'] = correction_rate
+
+
+@st.cache_data()
+def save_yesterday_data():
     current_time = datetime.now()
     current_time_utc = current_time.astimezone(pytz.utc)
     current_time_str = current_time_utc.isoformat()
     current_time_json_data = json.dumps(current_time_str)   
     data = {'start_time': current_time_json_data}
-    response_data = requests.get("http://localhost:8502/", data=json.dumps(data))
-    if response_data.status_code == 200:
-        st.write("Welcome")
+    response_data = requests.get("http://localhost:8502/save-yesterday", data=json.dumps(data))
 
+    if response_data.status_code == 200:
+        st.success('Updated until yesterday stats')
+
+
+def showFirstPage():
     row4 = st.columns(1)
     part1, _ = st.columns([4, 3])
-    show, chart = st.columns([1, 7])
+    placeholder = st.empty()
+    chart, line_chart = st.columns([4, 4])
     endtime_json_data = None 
     with row4[0]:
         st.markdown("<h3 style='text-align: right; color: black;'>Data retrieval period</h3>", unsafe_allow_html=True)
-        _, subcol1, subcol2, subcol3, subcol4 = st.columns([1,2,2,2,2])
+        _, subcol1, subcol2, subcol3, subcol4 = st.columns([4,1,1,1,1])
 
         with subcol1 :
             startdate = st.date_input('Select start date')
@@ -71,34 +109,53 @@ def showFirstPage():
                         print('Response content is not in JSON format.')
                         js = 'spam'
 
-
-    with show:
-        plot_button = show.button("Plot Chart", key='plot_daily_status')
-
-    with chart:
-        if plot_button:
-            data = {'start_time': starttime_json_data}
+    with placeholder.container():
+        chart_data, loaded_data = None, None
+        dates = [0]*30
+        with chart:
+            save_yesterday_data()
+            data = {'end_time': endtime_json_data}
             response_data = requests.get("http://localhost:8502/performance-statistics", data=json.dumps(data))
             if response_data.status_code == 200:
                 data = response_data.json()
-                daily_datas = data["dailyStatus"]
+                daily_datas = data["dailyStatus"]    
 
-                chart_data = {}         
-
-                dates = [0]*30
                 chart_data = np.zeros((30, 4))
+                loaded_data = [0]*30
                 for idx, daily_data in enumerate(daily_datas):
                     dates[idx] = daily_data["calc_date"][2:10]
-                    chart_data[idx][0] = daily_data["threshold_50"]
-                    chart_data[idx][1] = daily_data["threshold_30"]
-                    chart_data[idx][2] = daily_data["threshold_10"]
-                    chart_data[idx][3] = 100.0 - (daily_data["threshold_10"] + daily_data["threshold_30"] + daily_data["threshold_50"])
-                
-                columns = ["threshold_10", "threshold_30", "threshold_50", "threshold_over_50"]
+                    chart_data[idx][3] = daily_data["threshold_50"]
+                    chart_data[idx][2] = daily_data["threshold_30"]
+                    chart_data[idx][1] = daily_data["threshold_10"]
+                    chart_data[idx][0] = 100.0 - (daily_data["threshold_10"] + daily_data["threshold_30"] + daily_data["threshold_50"])
+                    loaded_data[idx] = daily_data["total_data"]
 
-                chart_data = pd.DataFrame(chart_data, columns=columns, index=dates)
+                chart_columns = ["threshold_10", "threshold_30", "threshold_50", "threshold_over_50"]
 
-                st.bar_chart(data=chart_data, x=None, y=None, width=0, height=1000, use_container_width=True)
+                chart_data = pd.DataFrame(chart_data, columns=chart_columns, index=dates)
+                loaded_data = pd.DataFrame({
+                                            'Date' : dates,
+                                            'Count': loaded_data
+                                            })
+
+                st.bar_chart(data=chart_data, x=None, y=None, width=0, height=500, use_container_width=True)
+
+        with line_chart:
+            tab1, tab2 = st.tabs(["Line Chart", "Loaded Data Histogram"])
+            with tab1:
+                st.header("Line Chart")
+                if chart_data.size != 0:
+                    st.line_chart(data=chart_data, x=None, y=None, width=0, height=500, use_container_width=True)
+            with tab2:
+                st.header("Loaded Data Histogram")
+                if loaded_data.size != 0:
+                    fig = px.histogram(data_frame=loaded_data, x='Date', y='Count', nbins=30, text_auto=True)
+                    st.write(fig)
+
+        st.markdown("### Detailed Data View")
+
 
 if __name__ == '__main__' :
+    set_page_title()
+    upload_today_data()
     showFirstPage()
